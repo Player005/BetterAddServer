ef73201b76bb589bdb9a02f64d2bfb822805daca
Player005 <player_005@yahoo.com>
2022-10-16 17:50:25 +0200

    add Inject to tick function + make it actually work after 2 hours of research

--- src/main/java/net/player005/betteraddserver/mixin/screen/MixinAddServerScreen.java
+++ src/main/java/net/player005/betteraddserver/mixin/screen/MixinAddServerScreen.java
@@ -1,4 +1,23 @@
 package net.player005.betteraddserver.mixin.screen;
 
-public class MixinAddServerScreen {
+import net.minecraft.client.gui.screen.AddServerScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import net.player005.betteraddserver.BetterAddServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AddServerScreen.class)
+public abstract class MixinAddServerScreen extends Screen {
+
+    public MixinAddServerScreen(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "tick",at = @At("HEAD"))
+    public void tick(CallbackInfo info){
+        BetterAddServer.LOGGER.info("Ticked!");
+    }
 }
